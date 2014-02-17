9499de37231b63a1d5bcd638ed88c8740a7dfb95
Carl Zulauf <carl@linkleaf.com>
2014-02-16 23:52:21 -0500

    Use placement sampler to gather stats instead of history

--- bin/placement_sampler.rb
+++ bin/placement_sampler.rb
@@ -12,13 +12,23 @@ load ARGV[0]
 
 player_class = Battleship::Util.find_player_classes.first
 
-placements = 100.times.map{ player_class.new.new_game }
-File.open("contestants/lib/bender/game_history.rb", "w") do |file|
-  file.write("module Bender\n  def self.game_history\n    [\n")
-  placements.each do |board|
-    file.write("      #{board.inspect},\n")
+stats = Hash.new(0)
+sample_size = 10_000
+sample_size.times do
+  player_class.new.new_game.each do |ship_info|
+    ship = Bender::Ship.new(*ship_info)
+    ship.points.each do |xy|
+      stats[xy] += 1
+    end
   end
-  file.write("    ]\n  end\nend\n")
+end
+
+stats.each_pair{ |xy, score| stats[xy] = ( score / sample_size.to_f * 100 ).round }
+
+File.open("contestants/lib/bender/game_stats.rb", "w") do |file|
+  file.write("module Bender\n  def self.game_stats\n")
+  file.write(stats.pretty_inspect.lines.map{ |l| "    #{l}" }.join)
+  file.write("  end\nend\n")
 end
 
 # binding.pry
@@ -26,7 +36,7 @@ end
 load "contestants/lib/bender/game_history.rb"
 
 clean_board = (0..9).map{ |y| (0..9).map{ :unknown } }
-bender = Bender::Game.new([:HistoryBonus])
+bender = Bender::Game.new("StatBonus" => 1)
 bender.update(clean_board, [5,4,3,3,2])
 bender.run_scores
 puts bender.board.inspect
