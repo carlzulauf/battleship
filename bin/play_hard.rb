582d6ffbd91948e046fb67077239227ab8483612
Carl Zulauf <carl@linkleaf.com>
2014-02-17 03:20:10 -0500

    Improve and simplify play hard output

--- bin/play_hard.rb
+++ bin/play_hard.rb
@@ -9,29 +9,18 @@ require "battleship/util"
 load ARGV[0]
 load ARGV[1]
 
-win_counts = Hash.new(0)
 player_classes = Battleship::Util.find_player_classes
+win_counts = {}.tap{ |c| player_classes.each{ |klass| c[klass.new.name] = 0 } }
 
-puts
 loop do
-  # puts win_counts.inspect if i % 10 == 0
   players = player_classes.map{ |klass| klass.new }
   game = Battleship::Game.new(10, [2, 3, 3, 4, 5], *players)
   game.tick until game.winner
   win_counts[game.winner.name] += 1
-  (winner, score1), (loser, score2) = win_counts.sort_by{ |name, c| c * -1 }
-  if score1 && score2
-    lead = (score1 - score2).to_s.rjust(8)
-    lead += "  (#{score1.to_s.rjust(8)} to #{score2.to_s.rjust(8)})"
-    name_size = [winner.length, loser.length].max
-    if score1 == score2
-      print "\r#{'Tied'.ljust(name_size)}        #{lead}"
-    else
-      print "\r#{winner.ljust(name_size)} up by: #{lead}"
-    end
-    break if score1 - score2 == 100
-  end
+  diff = win_counts.values.reduce(:-).abs
+  print "\r#{win_counts.inspect} (#{diff.to_s.rjust(3)} game lead)"
+  break if diff >= 100
+  players.reverse!
 end
 
 puts
-puts win_counts.inspect
